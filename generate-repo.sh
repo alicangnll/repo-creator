887caf381529ce82a81217b51d6453c996670567
Ali Can Gönüllü <alicangonullu1907@gmail.com>
2020-02-14 23:59:23 +0300

    Update generate-repo.sh

--- generate-repo.sh
+++ generate-repo.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
-echo Status: 200 Okay
+clear
+echo Durum: 200 Okay
 echo Content-Type: text/plain
-echo
-echo Rebuilding APT repository:
+echo ---------------------------------
+echo Gerekli Dosyalar Kuruluyor
+echo ---------------------------------
 
 sudo apt update
 sudo apt-get install dpkg-dev apache2 dpkg-sig
 /etc/init.d/apache2 start
+
+clear
+echo ---------------------------------
+echo Konum Ve GPG Geçmişi Siliniyor
+echo ---------------------------------
 
 cd /var/www/html
 mkdir debs 
@@ -20,10 +27,20 @@ find ~/.gnupg -type d -exec chmod 700 {} \;
 echo "cert-digest-algo SHA256" >> ~/.gnupg/gpg.conf
 echo "digest-algo SHA256" >> ~/.gnupg/gpg.conf
 
+clear
+echo ---------------------------------
+echo GPG Başlıyor
+echo ---------------------------------
+
 gpg --list-keys
 gpg --gen-key
 gpg --output FileName.gpg --armor --export $KEYID
-dpkg-sig --sign *.deb
+dpkg-sig --sign debs/*.deb
+
+
+echo ---------------------------------
+echo Repo Kaydı Yapılıyor
+echo ---------------------------------
 
 apt-ftparchive packages debs > Packages
 gzip -c Packages > Packages.gz
@@ -32,6 +49,18 @@ apt-ftparchive release debs > Release
 gpg --clearsign -o InRelease Release
 gpg -abs -o Release.gpg Release
 
+clear
+echo ---------------------------------
+echo Repo Ekleniyor
+echo ---------------------------------
+
 echo "deb http://127.0.0.1 /" | sudo tee -a /etc/apt/sources.list
 wget -q -O - http://127.0.0.1/FileName.gpg | sudo apt-key add -
 apt update
+
+clear
+echo ---------------------------------
+cat /etc/apt/sources.list
+echo ---------------------------------
+echo Kontrol Ediniz !!
+echo ---------------------------------
