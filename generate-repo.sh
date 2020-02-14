693fc193d619637fa5f0d702779c1a40c7ab7f1c
Ali Can Gönüllü <alicangonullu1907@gmail.com>
2020-02-14 23:49:33 +0300

    Update generate-repo.sh

--- generate-repo.sh
+++ generate-repo.sh
@@ -4,7 +4,7 @@ sudo apt-get install dpkg-dev apache2 dpkg-sig
 
 cd /var/www/html
 mkdir debs 
-rm -r InRelease ${KEYNAME}.gpg Packages Packages.gz Release Release.gpg
+rm -r InRelease FileName.gpg Packages Packages.gz Release Release.gpg
 rm -r ~/.gnupg/
 mkdir ~/.gnupg/
 chown -R $(whoami) ~/.gnupg/
@@ -16,16 +16,16 @@ echo "digest-algo SHA256" >> ~/.gnupg/gpg.conf
 
 gpg --gen-key
 gpg --list-keys
-gpg --output ${KEYNAME}.gpg --armor --export $KEYID
-dpkg-sig --sign debs/*.deb
+gpg --output FileName.gpg --armor --export $KEYID
+dpkg-sig --sign *.deb
 
 apt-ftparchive packages debs > Packages
-dpkg-scanpackages debs /dev/null | gzip -9c > Packages.gz
+gzip -c Packages > Packages.gz
 apt-ftparchive release debs > Release
 
 gpg --clearsign -o InRelease Release
 gpg -abs -o Release.gpg Release
 
 echo "deb http://127.0.0.1 /" | sudo tee -a /etc/apt/sources.list
-wget -q -O - http://127.0.0.1/${KEYNAME}.gpg | sudo apt-key add -
+wget -q -O - http://127.0.0.1/FileName.gpg | sudo apt-key add -
 apt update
